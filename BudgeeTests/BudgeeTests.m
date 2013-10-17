26173a6996d62b265efa4ebc4cf5a26f5873b61a
Wes Gibbs <wesgibbs@gmail.com>
2013-10-17 11:02:11 -0700

    Initial commit

---
+++ BudgeeTests/BudgeeTests.m
@@ -0,0 +1,34 @@
+//
+//  BudgeeTests.m
+//  BudgeeTests
+//
+//  Created by Wes Gibbs on 10/17/13.
+//  Copyright (c) 2013 Wes Gibbs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BudgeeTests : XCTestCase
+
+@end
+
+@implementation BudgeeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, :deployment_target => '6.0'
+
+xcodeproj 'Budgee.xcodeproj'
+
+pod 'RestKit'
